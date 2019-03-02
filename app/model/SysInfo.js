64af0840c383c1fad44d559e09ba41ddeb81a905
高承毅 <gaochengyidlmu@163.com>
2019-03-02 13:35:04 +0800

    初始化

---
+++ app/model/SysInfo.js
@@ -0,0 +1,21 @@
+// {app_root}/app/model/user.js
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
+
+  const SysInfoSchema = new Schema(
+    {
+      website: { type: String, default: '' }, // 网址
+      keywordList: { type: [String], default: [] }, // 首页中需要爬取的关键词列表
+      keyword: { type: String, default: '' }, // 当前正在查找的 keyword
+      pageNum: { type: Number, default: 1 }, // 当前查找对应的 page 页
+      maxPageNum: { type: Number, default: 15 }, // 最大查找页数
+      status: { type: Number, default: 0 }, // 0：未开始查找；1: 正在查找；2: 查询结束
+    },
+    {
+      timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
+    },
+  );
+
+  return mongoose.model('SysInfo', SysInfoSchema);
+};
---
+++ config/config.default.js
@@ -0,0 +1,33 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1551496689699_9787';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  config.mongoose = {
+    client: {
+      url: 'mongodb://127.0.0.1:27017/fabricSpider',
+      options: {
+        useNewUrlParser: true,
+        poolSize: 10,
+        useCreateIndex: true,
+      },
+    },
+  };
+
+  return config;
+};
